c13bc93f0bd2b5f633b0d4dbd0495432a2a9ccb1
Ahmed <ahmedlight@yandex.com>
2022-05-27 09:26:09 +0100

    build Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,10 +11,17 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 
 const SideBar = () => {
-  const {activeMenu , setActiveMenu } = useStateContext();
+  const {activeMenu , setActiveMenu  , screenSize} = useStateContext();
  
-const activeLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 '
-const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md m-2 text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
+ const handleCloseSideBar =() => {
+   if(activeMenu && screenSize <= 900) {
+     setActiveMenu(false)
+   }
+ }
+
+
+ const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
+ const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className='h-screen pb-10 ml-3 overflow-auto md:overflow-hidden md:hover:overflow-auto'>
 
@@ -22,7 +29,7 @@ const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md 
   activeMenu && <>
   <div className='flex items-center justify-between'>
     
-   <Link to="/" onClick={() => setActiveMenu(false)} 
+   <Link to="/" onClick={handleCloseSideBar} 
    className='flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900 '
    >
       <SiShopware /> <span>Shoppy</span>
@@ -31,6 +38,7 @@ const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md 
 
    <TooltipComponent content='Menu' position="BottomCenter">
      <button  type='button' 
+     
      onClick={()=>setActiveMenu((prevActiveMenu) => !prevActiveMenu )}
      className="block p-3 mt-4 text-xl rounded-full hover:bg-light-gray md:hidden"
      
@@ -52,10 +60,10 @@ const normalLink= 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-md 
          <NavLink
          to={`${item.name}`}
          key={item.name}
-         onClick={()=> {}}
-         className={({isActive}) => 
-          isActive ? activeLink : normalLink
-        }
+         onClick={handleCloseSideBar}
+         className={({isActive}) => ( isActive ? activeLink : normalLink)}
+          
+        
          >
   {item.icon}
   <span className='capitalize'> 
